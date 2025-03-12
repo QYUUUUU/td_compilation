9c7012f123a4c3e985652d10ee1f0fe93c430534
Jules <jules.holder.54@gmail.com>
2025-03-12 10:40:36 +0100

    fix TantQueG

--- src/main/java/fr/ul/miashs/compil/generation/TantQueG.java
+++ src/main/java/fr/ul/miashs/compil/generation/TantQueG.java
@@ -1,23 +1,22 @@
 package fr.ul.miashs.compil.generation;
 
-import fr.ul.miashs.compil.arbre.*;
-import fr.ul.miashs.compil.tds.TDS;
+import fr.ul.miashs.compil.arbre.Bloc;
+import fr.ul.miashs.compil.arbre.Noeud;
+import fr.ul.miashs.compil.arbre.TantQue;
 
-public class TantQueG {
+public class TantQueG extends InstructionG {
 
     public StringBuilder exp = new StringBuilder();
 
     public StringBuilder genererTantQue(TantQue noeud) {
         this.exp.append("\tCMP(R1,R2)\n");
 
-        Noeud condition = noeud.getCondition();
-        Bloc instructions = noeud.getBloc();
-        ExpressionG expressionG= new ExpressionG();
-        expressionG.generer_expression(condition, condition.getLabel());
+        Noeud condition = noeud.getCondition();//Récupération de la condition
+        Bloc blocInstructions = noeud.getBloc();//Récupération du bloc d'instructions à faire
+        ExpressionG expressionG = new ExpressionG();
+        expressionG.generer_expression(condition, condition.getLabel());//Génération de la condition
         InstructionG instructionG = new InstructionG();
-        instructionG.generer_instruction(instructions);
-
-
+        instructionG.generer_instruction(blocInstructions);//Génération du bloc d'instructions
         return this.exp;
     }
 }
